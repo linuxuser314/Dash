7dcad76af413530b97294f87a1ed097859dc7f17
avr_dude <creed_truman77@my.albemarle.edu>
2026-04-23 15:34:22 -0400

    failing

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,13 @@ int main(void){
 	initSerial9600();
 	initTimer2Millis();
 	initTimer0PWM();
-	
+
 	msgStruct tempMsg = {'\0', 0};
 	while(true){
 		tempMsg = readSerialMsg();
 		if(tempMsg.id != '\0'){
 			serialTransmitMsg(tempMsg.id, tempMsg.val);
 		}
+		myDelay(10);
 	}
 }
